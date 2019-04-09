d12392d7d467135cc662b613a91b4d33893fc415
user <portovin@yandex.ru>
2019-04-09 12:37:06 +0300

    markdown and 404

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
+
+import markdown
 
 # Create your views here.
+
+#def detail(request, question_id):
+#	return HttpResponse("You're looking at question %s." % question_id)
+
+def detail(request):
+	return HttpResponse("You're looking at question %s." % 10)
+
+def item(request,item_id):
+	try:
+		i = Item.objects.get(name = item_id)
+	except Item.DoesNotExist:
+		return HttpResponse("Item not found")
+	return HttpResponse(markdown.markdown(i.body,extensions=["tables"]))
